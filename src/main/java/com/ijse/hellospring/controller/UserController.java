0db3217ac4833aa2a36e9a62ff3b51573777a808
Bimsara Walallawita <bwalallawita@gmail.com>
2024-02-17 13:24:24 +0530

    Spring Rest Example

---
+++ src/main/java/com/ijse/hellospring/controller/UserController.java
@@ -0,0 +1,22 @@
+package com.ijse.hellospring.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ijse.hellospring.entity.User;
+import com.ijse.hellospring.service.UserService;
+
+@RestController
+public class UserController {
+    
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/users")
+    public List<User> getAllUsers() {
+        return userService.getAllUsers();
+    }
+}
---
+++ src/main/java/com/ijse/hellospring/service/UserService.java
@@ -0,0 +1,12 @@
+package com.ijse.hellospring.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.ijse.hellospring.entity.User;
+
+@Service
+public interface UserService {
+    List<User> getAllUsers();
+}
